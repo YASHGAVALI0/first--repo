c715db5f0c22fd5d7a631b1ae4e0e330891217a0
YASH GAVALII <yashhh251@gmail.com>
2025-08-21 18:53:17 +0530

    write a new code

--- hello.py
+++ hello.py
@@ -1,1 +1,18 @@
-print("hello git ")
+print(" hello word: ")
+
+num1 = 5
+num2 = 24
+sum = num1 + num2 
+print(sum) 
+
+name = "yash"
+print("hello",name)
+
+num = -3
+
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
+else:
+    print("Zero")
